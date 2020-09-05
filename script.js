64a4ca4dc3b8753cdc13993a5f28536410bd4444
Santiago Betancur Graciano <santybg_94@hotmail.com>
2020-09-04 22:56:24 -0500

    the first version of my portfolio saved in github

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector(".menuButton").addEventListener("click", () => {
+    document.querySelector(".navMenu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.showcase', { delay: 500 });
+
+ScrollReveal().reveal('.newsCards');
+ScrollReveal().reveal('.newsCards', { delay: 500 });
+
+ScrollReveal().reveal('.bannerOne');
+ScrollReveal().reveal('.bannerOne', { delay: 500 });
+
+ScrollReveal().reveal('.bannerTwo');
+ScrollReveal().reveal('.bannerTwo', { delay: 500 });
