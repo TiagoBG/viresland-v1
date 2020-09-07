95ef36e62f1ca9e00ca52a8d61714cafa250bfc4
Santiago Betancur Graciano <santybg_94@hotmail.com>
2020-09-06 21:40:11 -0500

    Ahora las secciones Aristtae y Contact estan terminadas

---
+++ aristtaeScript.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.showcase', { delay: 500 });
+
+ScrollReveal().reveal('.sectionA');
+ScrollReveal().reveal('.sectionA', { delay: 500 });
+
+ScrollReveal().reveal('.sectionB');
+ScrollReveal().reveal('.sectionB', { delay: 500 });
+
+ScrollReveal().reveal('.social');
+ScrollReveal().reveal('.social', { delay: 500 });
